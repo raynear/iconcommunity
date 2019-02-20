87b9ba30a0802b89294fdbc5454319bc7a0a6e2f
2infiniti <30359168+2infiniti@users.noreply.github.com>
2019-02-20 13:46:33 +0800

    remove iconsensus

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
-    path('iconsensus/', include('iconsensus.urls')),
+    #path('iconsensus/', include('iconsensus.urls')),
     path('admin/', admin.site.urls),
 ]
 
