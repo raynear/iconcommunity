cd1546943a9bda3b8681b06af08a0285ea32b248
2infiniti <2infiniti@icon.foundation>
2019-03-04 22:50:13 -0800

    adding resources section

--- core/settings.py
+++ core/settings.py
@@ -15,8 +15,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'iconsensus',
+
     'dashboard',
+    'resources',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
-    #path('iconsensus/', include('iconsensus.urls')),
+    path('resources/', include('resources.urls')),
     path('admin/', admin.site.urls),
 ]
 
