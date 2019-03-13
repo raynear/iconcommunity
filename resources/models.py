59d1063d1378255b97141ae4c2db987b239b2eaa
2infiniti <2infiniti@icon.foundation>
2019-03-12 20:17:22 -0700

    press release model

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+
+class PressRelease(models.Model):
+    #Logo, Site, Title, Link, Date, Display, Created, Updated
+    #pr_logo = models.
+    pr_site = models.CharField(max_length=200, default='')
+    pr_link = models.URLField(max_length=500, default='')
+    pr_date = models.DateField(_("Date"), default=datetime.date.today)
+    pr_display = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
