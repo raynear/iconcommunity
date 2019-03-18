20cc3d8c76b6284fbe8e84b0c1ded014652fa2a9
2infiniti <2infiniti@icon.foundation>
2019-03-18 00:30:21 -0700

    digg pagination ajax 100%

--- resources/views.py
+++ resources/views.py
@@ -1,35 +1,30 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import News, Press
+from el_pagination.decorators import page_template
 
 
 def collateral(request):
     return render(request, 'resources/collateral.html', {'nightmode': request.session['nightmode']})
 
 
-def press(request):
+'''
+def press2(request):
     news = News.objects.all().order_by('-news_date')
+    print(news)
     presses = Press.objects.all().order_by('-press_date')
-
-    ''' Working version
-    paginator = Paginator(news, 2)
-    page = request.GET.get('page1')
-    try:
-        news = paginator.page(page)
-    except PageNotAnInteger:
-        news = paginator.page(1)
-    except EmptyPage:
-        news = paginator.page(paginator.num_pages)
-
-    paginator = Paginator(presses, 2)
-    page = request.GET.get('page2')
-    try:
-        presses = paginator.page(page)
-    except PageNotAnInteger:
-        presses = paginator.page(1)
-    except EmptyPage:
-        presses = paginator.page(paginator.num_pages)
-
+    print(presses)
     return render(request, 'resources/press.html', {'news': news, 'presses': presses, 'nightmode': request.session['nightmode']})
-    '''
-    return render(request, 'resources/press.html', {'news': news, 'presses': presses, 'nightmode': request.session['nightmode']})
+'''
+
+
+@page_template('resources/news_page.html', key='news_page')
+@page_template('resources/press_page.html', key='press_page')
+def press(request, template='resources/press.html', extra_context=None):
+    context = {
+        'news': News.objects.all().order_by('-news_date'),
+        'presses': Press.objects.all().order_by('-press_date'),
+        'nightmode': request.session['nightmode'],
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return render(request, template, context)
