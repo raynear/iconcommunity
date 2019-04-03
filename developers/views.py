76c7d983ffcc9d18152f7cfdadceec1298944a5d
2infiniti <2infiniti@icon.foundation>
2019-04-03 15:46:30 +0900

    developers page update

--- developers/views.py
+++ developers/views.py
@@ -3,16 +3,21 @@ from .models import BlogTutorial, VideoTutorial
 from el_pagination.decorators import page_template
 
 
-
-@page_template('resources/news_page.html', key='news_page')
-def press(request, template='resources/press.html', extra_context=None):
+@page_template('resources/blog_tutorial_page.html', key='news_page')
+@page_template('resources/video_tutorial_page.html', key='news_page')
+def developers(request, template='developers/developers.html', extra_context=None):
     if 'nightmode' not in request.session:
         request.session['nightmode'] = False
     if 'navbar' not in request.session:
         request.session['navbar'] = True
+
+    video_tutorials = VideoTutorial.objects.all().order_by('-video_tutorial_date')
+    for url in video_tutorials:
+        url.video_tutorial_link = url.video_tutorial_link.replace("watch?v=", "embed/")+"?rel=0"
+
     context = {
-        'news': News.objects.all().order_by('-news_date'),
-        'presses': Press.objects.all().order_by('-press_date'),
+        'blog_tutorials': BlogTutorial.objects.all().order_by('-blog_tutorial_date'),
+        'video_tutorials': video_tutorials,
         'nightmode': request.session['nightmode'],
         'navbar': request.session['navbar'],
     }
