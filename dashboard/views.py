62e95ae1411f42c1c9d46a1fe861e12bad7d90bc
2infiniti <30359168+2infiniti@users.noreply.github.com>
2019-07-30 20:00:17 +0800

    add real time coinmarketcap data

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,52 @@
 from django.shortcuts import render
 
+# icon sdk test
+from iconsdk.icon_service import IconService
+from iconsdk.providers.http_provider import HTTPProvider
+
+# coinmarketcap test
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
 
 def index(request):
     if 'nightmode' not in request.session:
         request.session['nightmode'] = False
     if 'navbar' not in request.session:
         request.session['navbar'] = True
-    return render(request, 'dashboard/dashboard.html', {'section': 'DASHBOARD', 'nightmode': request.session['nightmode'], 'navbar': request.session['navbar']})
+
+    # ICON SDK test
+    #icon_service = IconService(HTTPProvider("https://bicon.net.solidwallet.io/api/v3"))
+    #latestblock = icon_service.get_block("latest")
+
+    # coinmarketcap (conversion for multiple currencies requires paid account, pay and refactor this later)
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    parameters = {
+        'id': '2722', #AC3
+        'convert': 'ICX'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': 'abd96001-925b-40d1-8160-9e02a66e7f5a',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        ac3data = data["data"]["2722"]
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
+    context = {
+        'nightmode': request.session['nightmode'],
+        'navbar': request.session['navbar'],
+        'section': 'DASHBOARD',
+        'ac3data': ac3data,
+    }
+
+    return render(request, 'dashboard/dashboard.html', context)
 
