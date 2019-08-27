08e9b3d2aaeb604cf8a9704f49eca39cf7b16753
raynear <raynear@gmail.com>
2019-08-27 18:01:24 +0900

    first commit

--- prep/views.py
+++ prep/views.py
@@ -27,8 +27,8 @@ def registration(request, template='prep/registration.html'):
 
 def get_prep(request):
     params = {
-        'address': request.session['fromAddress']
-        #'address': 'hxb2ed93806e9585a7a8b722f7031323925914de91'
+        # 'address': request.session['fromAddress']
+        'address': 'hxb2ed93806e9585a7a8b722f7031323925914de91'
     }
     response = None
     try:
@@ -60,15 +60,22 @@ def management(request, template='prep/management.html'):
         'getPRep': getPRep
     })
 
-    #context.update({
+    # context.update({
     #    'latest_block': latest_block,
-    #})
+    # })
 
     return render(request, template, context)
 
 
 def governance(request, template='prep/governance.html'):
     context = init_mode(request)
+    PReps = get_preps(request)
+    PRep = get_prep(request)
+    context["dataList"] = PReps['preps']
+    context["irep"] = int(PRep['irep'], 0)
+    context["irepUpdateBlockHeight"] = int(PRep['irepUpdateBlockHeight'], 0)
+    print(context["irep"])
+    print(context["irepUpdateBlockHeight"])
     return render(request, template, context)
 
 
