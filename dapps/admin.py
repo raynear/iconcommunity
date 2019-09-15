5362f67cd08303134bd3652378a76afb786f8515
2infiniti <30359168+2infiniti@users.noreply.github.com>
2019-09-15 11:07:39 +0800

    update dapps

--- dapps/admin.py
+++ dapps/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TXChallenge, OfficialDapps
+from .models import TXChallenge, OfficialDapps, Discover
 
 admin.site.register(TXChallenge)
 admin.site.register(OfficialDapps)
+admin.site.register(Discover)
