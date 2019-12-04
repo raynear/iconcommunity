48ad87f38b887fe6f1737cfcf3f1711920c17063
raynear <raynear@gmail.com>
2019-12-05 00:35:01 +0900

    bug fix

--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'iconcommunity',
         'USER': 'iconcommunityuser',
         'PASSWORD': '@icon111',
